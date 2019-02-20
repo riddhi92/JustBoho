5656e655331ea2e1629ba737fe3c629b2cb0ad40
Riddhi Patil <riddhi.patil92@gmail.com>
2019-02-20 15:56:22 -0500

    Initial working prototype of static pages

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+function updatePage() {
+ window.location.href = "../html/item.html";
+}
+
+function deletePage() {
+ window.location.href = "../html/myitems.html";
+}  
+
+function SaveItem() {
+    window.location.href = "../html/myitems.html";
+}   
